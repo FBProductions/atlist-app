50388736f56d23b1ef264ae29a71bf07fb700272
Erik Dahlin <dahlin140@gmail.com>
2018-09-17 23:55:22 -0500

    added login background

--- App.js
+++ App.js
@@ -1,45 +1,71 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Dimensions, Animated } from 'react-native';
 import { Button, Input } from 'react-native-elements';
 import Icon from 'react-native-vector-icons/FontAwesome';
-import Video from 'react-native-video';
-import loginVideo from './assets/fullsize_main.mp4'
+import { Video } from 'expo';
+import loginVideo from './assets/fullsize_main.mp4';
+
 
 export default class Login extends Component {
 
 	constructor(props) {
 		super(props);
 		this.state = { 
+			backgroundOpacity: new Animated.Value(0),
 			email: null,
 			password: null,
 			emailErr: false,
 			pwErr: false };
 	}
+
+	fadeInVideo = () => {
+		setTimeout(() => {
+		  Animated.spring(
+			this.state.backgroundOpacity,
+			{
+			  toValue: 1
+			}
+		  ).start();
+		}, 100); 
+	  }
 	
   render() {
     return (
 		<View style={styles.container}>
-			<Video
-				repeat
-				source={loginVideo}
-				resizeMode="cover"
-			/>
-			<Input
-				placeholder='Email'
-				leftIcon={{ type: 'font-awesome', name: 'envelope' }}
-				onChangeText={(email) => this.setState({email: email})}
-			/>
-			<Input
-				placeholder='Password'
-				leftIcon={<Icon name='lock'
-							size={34}/>}
-			/>
-			<Button title='Log in'
-				buttonStyle={{ width: 250, height: 50, backgroundColor: '#88c585' }}
-				containerStyle={{ margin: 20 }}
-			/>
 
-			<Text>{this.state.email}</Text>
+			<View style={styles.background}>
+				<Animated.View style={[styles.backgroundViewWrapper, {opacity: this.state.backgroundOpacity}]}>
+					<Video
+						source={loginVideo}
+						shouldPlay
+						resizeMode="cover"
+						style={styles.video}
+						isLooping
+						onLoad={() => this.fadeInVideo()}
+					/>
+				</Animated.View>
+			</View>
+
+			<View style={styles.overlay}> 
+				<View style={styles.login}> 
+				<Input
+					placeholder='Email'
+					placeholderTextColor='black'
+					leftIcon={{ type: 'font-awesome', name: 'envelope' }}
+					onChangeText={(email) => this.setState({email: email})}
+				/>
+				<Input
+					placeholder='Password'
+					placeholderTextColor='black'
+					leftIcon={<Icon name='lock'
+								size={34}/>}
+				/>
+				<Button title='Log in'
+					buttonStyle={{ width: 250, height: 50, backgroundColor: '#88c585' }}
+					containerStyle={{ margin: 20 }}
+				/> 
+				</View> 
+			</View>
 
 		</View>
     );
@@ -48,7 +74,36 @@ export default class Login extends Component {
 };
 
 const styles = StyleSheet.create({
-	container: { flex: 1, backgroundColor: '#85c5ae', alignItems: 'center', justifyContent: 'center' },
+	container: { flex: 1, backgroundColor: 'transparent', alignItems: 'center', justifyContent: 'center' },
+	video: {
+		width: Dimensions.get('window').width,
+		height: Dimensions.get('window').height
+	},
+	background: {
+		...StyleSheet.absoluteFillObject,
+		backgroundColor: '#000',
+	},
+	backgroundViewWrapper: {
+		...StyleSheet.absoluteFillObject,
+	},
+	overlay: {
+		...StyleSheet.absoluteFillObject,
+		backgroundColor: 'rgba(133,197,174,0.4)',
+		alignItems: 'center',
+		justifyContent: 'center'
+	},
+	login: {
+		backgroundColor: 'blue',
+		width: 350,
+		height: 200,//TODO
+		alignItems: 'center',
+		justifyContent: 'center',
+		backgroundColor: 'rgba(213, 217, 224, 0.8)',
+		borderRadius: 15
+	},
+	input: {
+		color: 'blue'
+	},
 	btns: { alignItems: 'center', justifyContent: 'center', backgroundColor: '#eee', 
 			padding: 10, flex: 1, height: 55 },
 			buttonPress: {
